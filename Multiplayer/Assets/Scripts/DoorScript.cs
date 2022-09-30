a6f85ce147d7e65b676b2f5fdfb51ae953d06139
Chakujin <cristiangalindo13@gmail.com>
2022-09-30 21:53:28 +0200

    Door

--- Multiplayer/Assets/Scripts/DoorScript.cs
+++ Multiplayer/Assets/Scripts/DoorScript.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
+    [SerializeField] private Animator m_animator;
     private static List<GameObject> currentPlayers;
 
-    private bool b_open = false;
+    [SerializeField]private bool b_open = false;
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Key")
         {
             b_open = true;
+            m_animator.SetBool("Open",true); // Animation
             Destroy(other);
-        }    
+        } 
+        if(other.tag == "Player" && b_open == true)
+        {
+            other.GetComponent<PlayerController>().DoorEnter();
+        }   
     }
 
+    //Add spawned players to total player list
     public static void UpdatePlayerTotal(GameObject player)
     {
         currentPlayers.Add(player);
--- Multiplayer/Assets/Scripts/Player/PlayerController.cs
+++ Multiplayer/Assets/Scripts/Player/PlayerController.cs
@@ -7,10 +7,14 @@ public class PlayerController : MonoBehaviour
 {
     private PhotonView m_View;
     public CharacterController2D controller;
+    [SerializeField] private SpriteRenderer m_myMesh;
+    [SerializeField] private BoxCollider2D[] m_myCol;
+    private Rigidbody2D m_rb;
 
-    [SerializeField]private float f_speed = 4f;
+    private const float f_speed = 70f;
     private bool b_jump = false;
     private bool b_crouch;
+    private bool b_move = true;
 
     private float f_horizontalMove;
     private float f_speedDir;
@@ -21,14 +25,15 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         m_View = GetComponent<PhotonView>();
+        m_rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (m_View.IsMine)
+        if (m_View.IsMine && b_move == true)
         {
-            f_horizontalMove = Input.GetAxisRaw("Horizontal") * f_speed;
+            f_horizontalMove = Input.GetAxisRaw("Horizontal") * f_speed; //Move axis
 
             if (Input.GetButtonDown("Jump") && b_jump == false)
             {
@@ -41,10 +46,24 @@ public class PlayerController : MonoBehaviour
             m_anim.SetFloat("Walk", f_speedDir);
         }
     }
+    
     private void FixedUpdate()
     {
         //Movement
         controller.Move(f_horizontalMove * Time.fixedDeltaTime, b_crouch, b_jump);
         b_jump = false;
+    }
+
+    public void DoorEnter()
+    {
+        m_myMesh.enabled = false;
+        b_move = false;
+
+        m_rb.bodyType = RigidbodyType2D.Static; //Enable gravity
+        
+        foreach(BoxCollider2D col in m_myCol)
+        {
+            col.enabled = false; //Enable collisions
+        }
     }
 }
