ad96737fa9a2718c36b36dda4598c9f5340c83f1
Chakujin <cristiangalindo13@gmail.com>
2022-10-01 01:31:39 +0200

    multiplayer scale sync

--- Multiplayer/Assets/Scripts/PhotonScripts/SpawnPlayers.cs
+++ Multiplayer/Assets/Scripts/PhotonScripts/SpawnPlayers.cs
@@ -12,6 +12,6 @@ public class SpawnPlayers : MonoBehaviour
         Vector2 randomPos = new Vector2(Random.Range(f_minX, f_maxX), Random.Range(f_minY, f_maxY));
         GameObject newPlayer =  PhotonNetwork.Instantiate(playerPrefab.name,randomPos, Quaternion.identity);
 
-        DoorScript.UpdatePlayerTotal(newPlayer); //Add player door list total players
+        //DoorScript.UpdatePlayerTotal(newPlayer); //Add player door list total players
     }
 }
