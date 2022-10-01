1f28b672b21714ec88a33b20faf208ca4e73b60e
Chakujin <cristiangalindo13@gmail.com>
2022-10-01 14:43:28 +0200

    Player jump

--- Multiplayer/Assets/Scripts/Player/PlayerController.cs
+++ Multiplayer/Assets/Scripts/Player/PlayerController.cs
@@ -5,21 +5,28 @@ using Photon.Pun;
 
 public class PlayerController : MonoBehaviour
 {
+    //Components
     private PhotonView m_View;
     public CharacterController2D controller;
+    private Rigidbody2D m_rb;
     [SerializeField] private SpriteRenderer m_myMesh;
     [SerializeField] private BoxCollider2D[] m_myCol;
-    private Rigidbody2D m_rb;
+    [SerializeField] private Animator m_anim;
 
+    //Movement
     private const float f_speed = 70f;
     private bool b_jump = false;
     private bool b_crouch;
     private bool b_move = true;
-
     private float f_horizontalMove;
     private float f_speedDir;
     public Vector2 lastPos;
-    [SerializeField] private Animator m_anim;
+
+    //Detect Players
+    [SerializeField] private Transform m_detectPos;
+    [SerializeField] private LayerMask m_playerLayer;
+    [SerializeField] private Vector2 f_sizeDetector;
+    private bool b_havePlayer = false;
 
     // Start is called before the first frame update
     void Start()
@@ -35,7 +42,7 @@ public class PlayerController : MonoBehaviour
         {
             f_horizontalMove = Input.GetAxisRaw("Horizontal") * f_speed; //Move axis
 
-            if (Input.GetButtonDown("Jump") && b_jump == false)
+            if (Input.GetButtonDown("Jump") && b_jump == false && b_havePlayer == false)
             {
                 lastPos = transform.position;
                 b_jump = true;
@@ -52,6 +59,16 @@ public class PlayerController : MonoBehaviour
         //Movement
         controller.Move(f_horizontalMove * Time.fixedDeltaTime, b_crouch, b_jump);
         b_jump = false;
+        b_havePlayer = false;
+
+        //Detect if I have a player on top of me 
+        Collider2D[] detectPlayers = Physics2D.OverlapBoxAll(m_detectPos.position, f_sizeDetector, 0, m_playerLayer);
+
+        foreach(Collider2D player in detectPlayers)
+        {
+            //If detect player cant jump
+            b_havePlayer = true;
+        }
     }
 
     public void DoorEnter()
@@ -65,5 +82,10 @@ public class PlayerController : MonoBehaviour
         {
             col.enabled = false; //Enable collisions
         }
+    }
+
+    private void OnDrawGizmos() 
+    {
+            Gizmos.DrawCube(m_detectPos.position, f_sizeDetector);
     }
 }
