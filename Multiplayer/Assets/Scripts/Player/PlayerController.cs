b7ae0977c8eb7749dc6fa6cf1f072272f842f563
Chaku <cristiangalindo13@gmail.com>
2022-10-03 10:40:21 +0200

    Player fix and animations

--- Multiplayer/Assets/Scripts/Player/PlayerController.cs
+++ Multiplayer/Assets/Scripts/Player/PlayerController.cs
@@ -18,15 +18,22 @@ public class PlayerController : MonoBehaviour
     private bool b_jump = false;
     private bool b_crouch;
     private bool b_move = true;
+    [SerializeField] private bool b_push = false;
     private float f_horizontalMove;
     private float f_speedDir;
-    public Vector2 lastPos;
+    public Vector2 lastPos; //Last pos before jump
 
     //Detect Players
     [SerializeField] private Transform m_detectPos;
     [SerializeField] private LayerMask m_playerLayer;
     [SerializeField] private Vector2 f_sizeDetector;
-    private bool b_havePlayer = false;
+    private bool b_havePlayer = false; //Detect if have player on my top
+
+    //Detect Objects
+    [SerializeField] private Transform m_detectObjPos;
+    [SerializeField] private LayerMask m_boxLayer;
+    [SerializeField] private Vector2 f_sizeDetectorObj;
+    [SerializeField]private bool b_haveBox = false;
 
     // Start is called before the first frame update
     void Start()
@@ -36,7 +43,8 @@ public class PlayerController : MonoBehaviour
 
         DieButton.RestartSceneCallback += RestartScene; //Event when press button
     }
-
+    
+    #region Updates
     // Update is called once per frame
     void Update()
     {
@@ -53,6 +61,16 @@ public class PlayerController : MonoBehaviour
             //Animation
             f_speedDir = Mathf.Abs(f_horizontalMove);
             m_anim.SetFloat("Walk", f_speedDir);
+            
+            //PushAnim
+            if (f_speedDir > 0 && b_push == true && b_haveBox == true)
+            {
+                m_anim.SetBool("Push", true);
+            }
+            else if (f_speedDir == 0 && b_push == true)
+            {
+                m_anim.SetBool("Push", false);
+            }
         }
     }
     
@@ -62,17 +80,45 @@ public class PlayerController : MonoBehaviour
         controller.Move(f_horizontalMove * Time.fixedDeltaTime, b_crouch, b_jump);
         b_jump = false;
         b_havePlayer = false;
+        b_haveBox = false;
 
         //Detect if I have a player on top of me 
         Collider2D[] detectPlayers = Physics2D.OverlapBoxAll(m_detectPos.position, f_sizeDetector, 0, m_playerLayer);
+        Collider2D[] detectObjects = Physics2D.OverlapBoxAll(m_detectObjPos.position, f_sizeDetectorObj, 0, m_boxLayer);
 
-        foreach(Collider2D player in detectPlayers)
+        foreach (Collider2D player in detectPlayers)
         {
             //If detect player cant jump
             b_havePlayer = true;
         }
-    }
 
+        foreach (Collider2D myObj in detectObjects)
+        {
+            //Detect Box
+            b_haveBox = true;
+        }
+    }
+    #endregion
+    
+    #region Collisions
+    //Animation when push box
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Box")
+        {
+            b_push = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Box")
+        {
+            b_push = false;
+            m_anim.SetBool("Push", false);
+        }
+    }
+    #endregion
+    
     #region Voids
     public void DoorEnter()
     {
@@ -97,8 +143,10 @@ public class PlayerController : MonoBehaviour
         m_anim.SetFloat("Walk", f_speedDir);
     }
     #endregion
+    
     private void OnDrawGizmos() 
     {
-            Gizmos.DrawCube(m_detectPos.position, f_sizeDetector);
+        Gizmos.DrawCube(m_detectPos.position, f_sizeDetector);
+        Gizmos.DrawCube(m_detectObjPos.position, f_sizeDetectorObj);
     }
 }
