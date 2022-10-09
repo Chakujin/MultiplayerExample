ca47f22f62d4e9414957d246b015ccf70d95dd59
Chaku <cristiangalindo13@gmail.com>
2022-10-09 17:50:55 +0200

    Audios Fix

--- Multiplayer/Assets/Scripts/Props/DoorScript.cs
+++ Multiplayer/Assets/Scripts/Props/DoorScript.cs
@@ -1,37 +1,45 @@
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.SceneManagement;
 
 public class DoorScript : MonoBehaviour
 {
     [SerializeField] private Animator m_animator;
-    [SerializeField] private  int i_currentPlayers;
-    [SerializeField] private int i_enterPlayers;
+    [SerializeField] private Vector2 f_sizeDetector;
+    [SerializeField] private LayerMask m_keyLayer;
+    
+    private  int i_currentPlayers = 0;
+    private int i_enterPlayers = 0;
 
-    [SerializeField]private bool b_open = false;
+    private bool b_open = false;
 
     private void Start() 
     {
         AddPlayer();
     }
 
+    private void FixedUpdate()
+    {
+        //DetectKey 
+        Collider2D[] detectKey = Physics2D.OverlapBoxAll(transform.position, f_sizeDetector, 0, m_keyLayer);
+
+        foreach (Collider2D key in detectKey)
+        {
+            TakeKey();
+            Destroy(key.gameObject);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        if(other.tag == "Key")
-        {
-            GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>().PlayRandomPitch("Door");
-            b_open = true;
-            m_animator.SetBool("Open",true); // Animation
-            AddPlayer(); //Take total players current ingame
-            Destroy(other);
-        } 
         if(other.tag == "Player" && b_open == true)
         {
             other.GetComponent<PlayerController>().DoorEnter();
-            i_currentPlayers++; // Add player total enter on the door
+            i_enterPlayers++; // Add player total enter on the door
             
             if(i_currentPlayers == i_enterPlayers) //If all players enter
             {
-                //Termina el nivel
+                PhotonNetwork.LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
             }
         }   
     }
@@ -39,6 +47,18 @@ public class DoorScript : MonoBehaviour
     private void AddPlayer()
     {
         i_currentPlayers = PhotonNetwork.PlayerList.Length;
-        Debug.Log(i_currentPlayers);
+    }
+
+    private void TakeKey()
+    {
+        GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>().Play("Door");
+        b_open = true;
+        m_animator.SetBool("Open", true); // Animation
+        AddPlayer(); //Take total players current ingame
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, f_sizeDetector);
     }
 }
